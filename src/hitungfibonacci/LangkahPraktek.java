75bfd2b63a2b7471d23fbacd1c69d0499586cc5b
ASUS-A442U <ASUS-A442U@LAPTOP-0B7VJDRL>
2018-09-13 14:42:08 +0700

    Tambah Hitung Fibonacci

--- src/hitungfibonacci/LangkahPraktek.java
+++ src/hitungfibonacci/LangkahPraktek.java
@@ -17,6 +17,7 @@ public class LangkahPraktek {
     
     tampilJudul(identitas);
     int n = tampilInput();
+    BigInteger hasil = fibo(n);
     }
     
     private static int tampilInput(){
@@ -26,5 +27,18 @@ public class LangkahPraktek {
         int n = scanner.nextInt();
         
         return n;
+    }
+    
+    private static BigInteger fibo (int n){
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for(int i = 2; i < n; i++)
+        {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        return hasil[n-1];
     }
 }
