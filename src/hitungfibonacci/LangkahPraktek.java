ed289f3bcd079cf657c59f16665995d4146c2e64
ASUS-A442U <ASUS-A442U@LAPTOP-0B7VJDRL>
2018-09-13 13:08:20 +0700

    tampilJudul

---
+++ src/hitungfibonacci/LangkahPraktek.java
@@ -0,0 +1,15 @@
+
+package hitungfibonacci;
+
+
+public class LangkahPraktek {
+
+   
+    public static void tampilJudul(String identitas) {
+        System.out.println("identitas :" + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst./n"); 
+    }
+    
+}
