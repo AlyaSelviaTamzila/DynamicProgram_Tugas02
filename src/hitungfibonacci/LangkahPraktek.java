5e3fbd40422d226e6281a668434bca12b4a6b027
ASUS-A442U <ASUS-A442U@LAPTOP-0B7VJDRL>
2018-09-13 13:12:21 +0700

    Initial Commit

--- src/hitungfibonacci/LangkahPraktek.java
+++ src/hitungfibonacci/LangkahPraktek.java
@@ -12,4 +12,9 @@ public class LangkahPraktek {
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst./n"); 
     }
     
+    public static void main(String args[]){
+    String identitas = "Alya Selvia Tamzila / XRPL2 / 03";
+    
+    tampilJudul(identitas);
+    }
 }
