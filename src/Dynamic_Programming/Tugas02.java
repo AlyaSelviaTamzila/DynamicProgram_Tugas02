5d933daa2403d7a6797544b71b2f4aa0be44ee7f
ASUS-A442U <ASUS-A442U@LAPTOP-0B7VJDRL>
2018-09-14 22:22:21 +0700

    Initial

--- src/Dynamic_Programming/Tugas02.java
+++ src/Dynamic_Programming/Tugas02.java
@@ -3,7 +3,7 @@ package Dynamic_Programming;
 import java.util.Scanner;
 public class Tugas02 {
     public static void main(String args[]){
-     String Identitas  = "Rafi Raihan F / X RPL 2 / 26";
+     String Identitas  = "Alya  Selvia Tamzila / X RPL 2 / 03";
         System.out.println("Identitas: " + Identitas);
     }
 }
