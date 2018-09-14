b17a9f00760dacabc1ffc724188299d5433b5fd0
ASUS-A442U <ASUS-A442U@LAPTOP-0B7VJDRL>
2018-09-14 22:06:11 +0700

    Tambah input

--- src/Dynamic_Programming/Tugas02.java
+++ src/Dynamic_Programming/Tugas02.java
@@ -6,6 +6,12 @@ public class Tugas02 {
         
         String Identitas  = "Alya Selvia Tamzila / X RPL 2 / 03";
         System.out.println("Identitas: " + Identitas);
+        
+        int [] berat = {7, 5, 4, 3, 2,};
+        int [] bayaran  = {10000, 7000, 5000, 3000, 2000};
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Jumlah berat: ");
+        int total = scan.nextInt();
      }
         
     
